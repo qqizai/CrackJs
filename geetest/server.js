8c23ac29d9c513547413eecf48bc83af6198be3e
qqizai <ldzyjf313@163.com>
2020-07-05 22:36:24 +0800

    update geetest

--- geetest/server.js
+++ geetest/server.js
@@ -1,5 +1,5 @@
 var express = require("express")
-var sdk = require("./encrypt.js")
+var sdk = require("E:\\datas\\python\\爬虫项目\\CrackJs\\geetest\\my_encrypt.js")
 var bodyParser = require('body-parser')
 
 var api = express()
@@ -11,16 +11,33 @@ api.use(bodyParser.urlencoded({
 }));
 
 api.get('/get_token', function(req, res){
-    var token = sdk.jeeq()
+    var token = sdk.my_token()
     res.send(token)
 });
 
+//获取参数n
+api.post('/encrypt_n', function(req, res){
+    var token = req.body.token;
+    var n = sdk.encrypt_n(token)
+    res.send(n)
+});
+
+//获取参数o
+api.post('/encrypt_o', function(req, res){
+    var gt = req.body.gt;
+    var challenge = req.body.challenge;
+    var token = req.body.token;
+    var o = sdk.encrypt_o(gt, challenge, token)
+    res.send(o)
+});
+
+//获取参数 w = o + n
 api.post('/encrypt', function(req, res){
     var gt = req.body.gt;
     var challenge = req.body.challenge;
     var token = req.body.token;
-    var token = sdk.encrypt(gt, challenge, token)
-    res.send(token)
+    var w = sdk.my_encrypt(gt, challenge, token)
+    res.send(w)
 });
 
 api.get("/", function (req, res) {
