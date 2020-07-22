37a40d62a72b6c8001be3427728a0d9b8d157639
e <ldzyjf313@163.com>
2020-07-22 22:13:36 +0800

    update dun163

--- dun163/fingerprint.js
+++ dun163/fingerprint.js
@@ -1,342 +1,135 @@
 
+function my_fp() {
+    var S = h = x(n)//S其实是等于 对象n的字符串形式，如："{'v':'v1.1','fp':'25662623634134,7139997656248','u':'D181595319310996V4r','h':'dun.163.com'}"
+        , n = se;
+    if (null == n || void 0 == n)
+        throw Error(u[73]);
+    null != S && void 0 != S || (S = l[0]);
+    var E, w = S;
+    E = o(null == S ? [] : d(S));
+    //d(S) 是一个将 字符串S进行URL编码，然后再转换为ascii码，最后返回一个数组，一个字符串作为一个元素，其实最后返回的数组是可以还原字符串的.
+    //再使用o()函数将其转换为指定的长度的加密后的数据
+    var R = d(w + E)
+        , k = d(n);
+    null == R && (R = []),
+        E = [];
+    // for (var C = t[9]; C < ae; C++) {
+    //     var O = Math.random() * t[295]
+    //         , O = Math.floor(O);
+    //     E[C] = b(O)
+    // }
+    for (var C = 0; C < 4; C++) {
+        var O = Math.random() * 256
+            , O = Math.floor(O);
+        E[C] = b(O)
+    }
 
+    var X, k = r(k), k = y(k, r(E)), C = k = r(k);
+    if (null == R || void 0 == R || R.length == 0)
+        X = f(ie);//ie=64
+    else {
+        var L = R.length
+            , V = 0
+            // , V = L % ie <= ie - oe ? ie - L % ie - oe : ie * t[10] - L % ie - oe
+            , V = L % 64 <= 64 - 4 ? 64 - L % 64 - 4 : 64 * 2 - L % 64 - 4
+            , O = [];
+        c(R, t[9], O, t[9], L);
+        for (var B = t[9]; B < V; B++)
+            O[L + B] = t[9];
+        c(j(L), t[9], O, L + V, oe),
+            X = O
+    }
+    if (L = X,
+    null == L || L.length % ie != t[9])
+        throw Error(u[83]);
+    X = [];
+    // for (var Y = t[9], F = L.length / ie, U = t[9]; U < F; U++) {
+    //     X[U] = [];
+    //     for (var z = t[9]; z < ie; z++)
+    //         X[U][z] = L[Y++]
+    // }
+    for (var Y = 0, F = L.length / 64, U = 0; U < F; U++) {
+        X[U] = [];
+        for (var z = 0; z < 64; z++)
+            X[U][z] = L[Y++]
+    }
 
-
-var l, u, f = n(4), c = f.INVOKE_HOOK, j = f.EVENT_RESET, d = f.SWITCH_TYPE, h = f.SET_TYPE, p = f.SWITCH_LOAD_STATUS, y = f.UPDATE_VERIFY_STATUS, v = f.REFRESH, g = f.UPDATE_COUNTS_OF_VERIFYERROR, b = f.SET_TOKEN, m = f.EVENT_RESET_CLASSIC, _ = n(12), w = _.FETCH_CAPTCHA, T = _.FETCH_INTELLISENSE_CAPTCHA, S = _.VERIFY_CAPTCHA, E = _.VERIFY_INTELLISENSE_CAPTCHA, R = _.RESET_STATE, k = n(3), C = k.CAPTCHA_TYPE, O = k.DEVICE, I = n(2), $ = n(1), X = n(16), x = n(8), A = x.eypt, P = n(36), N = n(7), M = N.createNetCollect, D = n(5), L = D.UNPASS_ERROR, V = D.REQUEST_API_ERROR, B = D.BUSINESS_ERROR, Y = I.isMobile ? O.TOUCH : I.supportTouch ? O.MOUSE_TOUCH : O.MOUSE, F = {
-    state: {
-        version: "2.14.0",
-        fingerprint: "",
-        config: null,
-        langPkg: null,
-        captchaType: null,
-        type: "",
-        load: null,
-        verifyStatus: "",
-        token: "",
-        previousToken: "",
-        countsOfVerifyError: 0
-    },
-    mutations: (l = {},
-        i(l, c, function() {}),
-        i(l, j, function() {}),
-        i(l, m, function() {}),
-        i(l, d, function(e, t) {
-            e.captchaType = t.captchaType
-        }),
-        i(l, h, function(e, t) {
-            e.type = t.captchaType
-        }),
-        i(l, p, function(e, t) {
-            e.load = t
-        }),
-        i(l, y, function(e, t) {
-            e.verifyStatus = t.verifyStatus
-        }),
-        i(l, v, function(e) {
-            e.load = null,
-                e.verifyStatus = ""
-        }),
-        i(l, g, function(e, t) {
-            e.countsOfVerifyError = t.counts
-        }),
-        i(l, b, function(e, t) {
-            t && (e.previousToken = t),
-                e.token = t
-        }),
-        l),
-    actions: (u = {},
-        i(u, R, function(e) {
-            var t = e.commit;
-            t(d, {
-                captchaType: null
-            }),
-                t(p, null),
-                t(y, {
-                    verifyStatus: ""
-                }),
-                t(b, ""),
-                t(g, {
-                    counts: 0
-                })
-        }),
-        i(u, w, function(e, t) {
-            var n = e.commit
-                , i = e.state
-                , a = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : function() {}
-                , l = i.fingerprint
-                , u = i.version
-                , f = i.$fetch
-                , j = i.captchaCollector
-                , y = i.config
-                , v = y.apiServer
-                , g = y.captchaId
-                , m = y.protocol
-                , _ = y.captchaType
-                , w = y.ipv6
-                , T = y.runEnv
-                , S = y.group
-                , E = y.scene
-                , R = Object.assign({
-                id: g,
-                fp: l,
-                https: "https" === m,
-                type: _,
-                version: u,
-                dpr: window.devicePixelRatio || 1,
-                dev: Y,
-                cb: s(),
-                ipv6: w,
-                runEnv: T,
-                group: S,
-                scene: E
-            }, t)
-                , k = r({
-                apiServer: v,
-                protocol: m
-            }, "/get");
-            n(p, {
-                status: "loading"
-            }),
-                f(k, R, function(e, t) {
-                    if (e = o(e, t, k)) {
-                        var r = new D(e.code,e.message,{
-                            url: k,
-                            api: "get"
-                        });
-                        return a(r),
-                            n(p, {
-                                status: "fail",
-                                data: r
-                            }),
-                            void n(c, {
-                                name: "onError",
-                                args: [r]
-                            })
-                    }
-                    a(null, t);
-                    var s = t.data;
-                    s.type !== C.INTELLISENSE && s.type !== i.captchaType && n(d, {
-                        captchaType: s.type,
-                        prevCaptchaType: i.captchaType
-                    }),
-                        n(h, {
-                            captchaType: s.type
-                        }),
-                        n(b, s.token),
-                        n(p, {
-                            status: "loading",
-                            data: s
-                        })
-                }, {
-                    onProcess: M(j)
-                })
-        }),
-        i(u, T, function(e, t) {
-            var n = e.commit
-                , i = e.state
-                , a = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : function() {}
-                , l = i.fingerprint
-                , u = i.version
-                , f = i.$fetch
-                , j = i.captchaCollector
-                , d = i.config
-                , p = d.apiServer
-                , y = d.captchaId
-                , v = d.protocol
-                , g = d.captchaType
-                , m = d.ipv6
-                , _ = d.runEnv
-                , w = d.group
-                , T = d.scene
-                , S = r({
-                apiServer: p,
-                protocol: v
-            }, "/get");
-            f(S, {
-                id: y,
-                fp: l,
-                https: "https" === v,
-                type: g,
-                width: t.width,
-                version: u,
-                dpr: window.devicePixelRatio || 1,
-                dev: Y,
-                cb: s(),
-                ipv6: m,
-                runEnv: _,
-                group: w,
-                scene: T
-            }, function(e, t) {
-                if (e = o(e, t, S)) {
-                    var i = new D(e.code,e.message,{
-                        url: S,
-                        api: "get"
-                    });
-                    return n(c, {
-                        name: "onError",
-                        args: [i]
-                    }),
-                        void a(i)
+    Y = [],
+        // c(E, t[9], Y, t[9], ae);
+        c(E, 0, [], 0, 4);//E就是前面生成的参数，可以写死，因为是随机生成的：本地生成：[-128, -127, -127, -128]；浏览器生成：[120, 41, 62, -96]
+    for (var K = X.length, G = t[9]; G < K; G++) {
+        var J, Q, Z = X[G];
+        if (null == Z)
+            Q = null;
+        else {
+            for (var ee = b(t[92]), F = [], te = Z.length, ne = t[9]; ne < te; ne++)
+                F.push(v(Z[ne], ee));
+            Q = F
+        }
+        var re;
+        if (F = Q,
+        null == F)
+            re = null;
+        else {
+            for (var ce = b(t[91]), U = [], je = F.length, ve = t[9]; ve < je; ve++)
+                U.push(v(F[ve], ce--));
+            re = U
+        }
+        if (F = re,
+        null == F)
+            J = null;
+        else {
+            for (var ge = b(t[110]), U = [], _e = F.length, we = t[9]; we < _e; we++)
+                U.push(g(F[we], ge++));
+            J = U
+        }
+        var Te, Se = y(J, k);
+        if (F = Se,
+            U = C,
+        null == F)
+            Te = null;
+        else if (null == U)
+            Te = F;
+        else {
+            for (var z = [], Ee = U.length, Re = t[9], ke = F.length; Re < ke; Re++)
+                z[Re] = b(F[Re] + U[Re % Ee]);
+            Te = z
+        }
+        var Se = y(Te, C)
+            , Ce = i(Se)
+            , Ce = i(Ce);
+        c(Ce, t[9], Y, G * ie + ae, ie),
+            C = Ce
+    }
+    var Oe;//这个是指纹的部分参数
+    if (null == Y || void 0 == Y)
+        Oe = null;
+    else if (Y.length == 0)
+        Oe = "";//Oe = l[0]
+    else {
+        var Ie = 3;
+        try {
+            // for (var K = [], $e = t[9]; $e < Y.length; ) {
+            for (var K = [], $e = 0; $e < Y.length; ) {
+                if (!($e + Ie <= Y.length)) {
+                    K.push(a(Y, $e, Y.length - $e));
+                    break
                 }
-                n(h, {
-                    captchaType: C.INTELLISENSE
-                }),
-                    n(b, t.data.token),
-                    a(null, t)
-            }, {
-                onProcess: M(j)
-            })
-        }),
-        i(u, E, function(e, t, n) {
-            var i = e.commit
-                , l = e.state
-                , u = l.version
-                , f = l.type
-                , j = l.$fetch
-                , d = l.captchaCollector
-                , h = l.config
-                , p = h.apiServer
-                , y = h.captchaId
-                , v = h.protocol
-                , g = h.extraData
-                , b = h.runEnv
-                , m = Object.assign({
-                id: y,
-                version: u,
-                cb: s(),
-                extraData: a(g),
-                runEnv: b
-            }, t)
-                , _ = r({
-                apiServer: p,
-                protocol: v
-            }, "/check");
-            j(_, m, function(e, t) {
-                if (e = o(e, t, _),
-                    e ? e = new D(e.code,e.message,{
-                        url: _,
-                        token: m.token,
-                        type: f
-                    }) : $.getIn(t, "data.result") || (e = new D(L,"Failed to verify captcha.",{
-                        url: _,
-                        type: f,
-                        token: m.token
-                    })),
-                    e)
-                    i(c, {
-                        name: "onVerify",
-                        args: [e]
-                    });
-                else {
-                    var r = l.fingerprint
-                        , a = P(A(t.data.validate + "::" + r));
-                    i(c, {
-                        name: "onVerify",
-                        args: [null, {
-                            validate: a
-                        }]
-                    })
-                }
-                n && n(e, t)
-            }, {
-                onProcess: M(d, {
-                    token: m.token
-                })
-            })
-        }),
-        i(u, S, function(e, t) {
-            var n = e.commit
-                , i = e.state
-                , l = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : function() {}
-                , u = i.fingerprint
-                , f = i.captchaType
-                , j = i.version
-                , d = i.verifyStatus
-                , h = i.countsOfVerifyError
-                , p = i.$fetch
-                , v = i.type
-                , b = i.captchaCollector
-                , m = i.config
-                , _ = m.apiServer
-                , w = m.captchaId
-                , T = m.protocol
-                , S = m.extraData
-                , E = m.runEnv
-                , R = t.token
-                , k = t.data
-                , O = t.width
-                , I = t.acToken
-                , $ = r({
-                apiServer: _,
-                protocol: T
-            }, "/check");
-            n(y, {
-                verifyStatus: "verifying"
-            });
-            var X = function(e, t) {
-                var i = t && t.data;
-                if (e = o(e, t, $),
-                    !(["success", "error"].indexOf(d) > -1)) {
-                    if (e || !i.result && f !== C.SMS) {
-                        var r = e ? e.message : "Failed to verify captcha."
-                            , a = e ? e.code : L;
-                        return e = new D(a,r,{
-                            url: $,
-                            token: R,
-                            type: v,
-                            counts: h + 1
-                        }),
-                            n(y, {
-                                verifyStatus: "error",
-                                error: e
-                            }),
-                            n(g, {
-                                counts: h + 1
-                            }),
-                            n(c, {
-                                name: "onVerify",
-                                args: [e]
-                            }),
-                            void l(e)
-                    }
-                    if (i.result) {
-                        var s = P(A(i.validate + "::" + u));
-                        n(y, {
-                            verifyStatus: "success",
-                            validate: i.validate
-                        }),
-                            n(c, {
-                                name: "onVerify",
-                                args: [null, {
-                                    validate: s
-                                }]
-                            }),
-                            l(null, t)
-                    }
-                }
-            };
-            p($, {
-                id: w,
-                token: R,
-                acToken: I,
-                data: k,
-                width: O,
-                type: f,
-                version: j,
-                cb: s(),
-                extraData: a(S),
-                runEnv: E
-            }, X, {
-                onProcess: M(b, {
-                    token: R
-                })
-            })
-        }),
-        u)
-};
+                K.push(a(Y, $e, Ie)),
+                    $e += Ie
+            }
+            // Oe = K.join(l[0])
+            Oe = K.join("")
+        } catch (Xe) {
+            throw Error(u[64])
+        }
+    }
+    h = Oe
 
 
 
+}
 
 
+console.log(my_fp())
 
